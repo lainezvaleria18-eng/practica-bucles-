a4858fc18efd0a9d8c7faa1ddb651aed85fbe3e1
Josue Garay <Usuario@DESKTOP-EHLDOSJ>
2026-05-18 21:56:57 -0600

    Ejercicio 6 Finalizado Josue

---
+++ src/Ejercicio6_MultiplosDeCinco.java
@@ -0,0 +1,9 @@
+public class Ejercicio6_MultiplosDeCinco {
+    public static void main(String[] args) {
+        System.out.println("Desplegando los primeros 50 múltiplos de 5:");
+
+        for (int i = 1; i <= 50; i++) {
+            System.out.println("Múltiplo #" + i + " = " + (i * 5));
+        }
+    }
+}
